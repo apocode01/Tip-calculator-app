b9abeac5b2d25afb92e235d8a32e35dd06019ed8
apocode01 <apopanou@gmail.com>
2025-08-26 18:02:45 +0300

    small tip input changes

--- index.js
+++ index.js
@@ -9,12 +9,25 @@ customTipButton.addEventListener('click', () => {
 
 tipInput.addEventListener("focus", () => {
     buttons.forEach(b => b.classList.remove(`active`));
+    tipInput.classList.remove(`active`)
+});
+
+tipInput.addEventListener("blur", () => {
+    if (tipInput.value.trim() === "") {
+        customTipButton.style.display = "inline-block";
+        tipInput.style.display = "none";
+    }
+    else {
+        tipInput.classList.add(`active`)
+    }
 });
 
 const buttons = document.querySelectorAll(`.fixedTipButton`);
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        customTipButton.style.display = "inline-block";
+        tipInput.style.display = "none";
         // Remove all active states
         buttons.forEach(b => b.classList.remove(`active`));
         // Add active state for the button pressed
